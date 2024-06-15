fa6fe1e7d900c7ce7a888667a3b4fbfb1e96b19b
teeminus <32164856+teeminus@users.noreply.github.com>
2024-06-15 08:51:33 +0200

    - Update to ungoogled-chromium 126.0.6478.55-1

--- build.py
+++ build.py
@@ -252,7 +252,7 @@ def main():
 
         # Generate version file
         with open(RUST_FLAG_FILE, 'w') as f:
-            f.write('rustc 1.79.0-nightly (a07f3eb43 2024-04-11)')
+            f.write('rustc 1.79.0-nightly (ef8b9dcf2 2024-04-24)')
             f.write('\n')
 
     if not args.ci or not (source_tree / 'out/Default').exists():
